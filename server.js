7df59d832d740e7b656cc1013b792d96c790a0db
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-11-09 12:51:11 +0530

    f

--- server.js
+++ server.js
@@ -23,12 +23,7 @@ const PORT = process.env.PORT || 5000
 websocketService.initialize(server)
 
 // Middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}))
+app.use(cors())
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ limit: "50mb", extended: true }))
 
